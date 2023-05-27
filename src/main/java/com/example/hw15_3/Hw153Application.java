f401f129de5912828f67e4a09658056fd7596bd1
IVAN <luka.news.2001@mail.ru>
2023-05-27 13:00:55 +0300

    Библиотеки 1.2

---
+++ src/main/java/com/example/hw15_3/Hw153Application.java
@@ -0,0 +1,13 @@
+package com.example.hw15_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw153Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw153Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hw15_3/Hw153ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw15_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw153ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
